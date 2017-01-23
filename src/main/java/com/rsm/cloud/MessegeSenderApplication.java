973c100a0b5596d769ff1b211a2832ad39ef661e
ChathuraHennayaka <hennayaka121@gmail.com>
2017-01-23 17:21:48 +0530

    Initial

---
+++ src/main/java/com/rsm/cloud/MessegeSenderApplication.java
@@ -0,0 +1,22 @@
+package com.rsm.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+
+
+@SpringBootApplication
+@EnableBinding(Source.class)
+public class MessegeSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessegeSenderApplication.class, args);
+	}
+	
+	@InboundChannelAdapter(Source.OUTPUT)
+	public String getMessage() {
+		return "Hello" + System.currentTimeMillis();
+	}
+}
